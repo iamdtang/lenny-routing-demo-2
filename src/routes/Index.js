17e5849edf3d5cecf28f02f90ac992d38d548e1e
David Tang <687449+iamdtang@users.noreply.github.com>
2023-04-29 22:30:49 -0700

    useSearchParams example

--- src/routes/Index.js
+++ src/routes/Index.js
@@ -1,13 +1,29 @@
-import { useLoaderData, Form, Link } from "react-router-dom";
+import { useLoaderData, useSearchParams, Form, Link } from "react-router-dom";
 
 export default function Index() {
   const hobbies = useLoaderData();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  let filteredHobbies = hobbies;
+
+  if (searchParams.has("q")) {
+    const q = searchParams.get("q");
+
+    filteredHobbies = hobbies.filter((hobby) => {
+      return hobby.name.toLowerCase().includes(q.toLowerCase());
+    });
+  }
 
   return (
     <div>
       <h1>Hobbies</h1>
+
+      <Form method="get">
+        <input type="search" placeholder="Search for a hobby" name="q" />
+      </Form>
+
       <ul>
-        {hobbies.map((hobby) => {
+        {filteredHobbies.map((hobby) => {
           return (
             <li key={hobby.id}>
               <Link to={`/hobbies/${hobby.id}`}>{hobby.name}</Link>
