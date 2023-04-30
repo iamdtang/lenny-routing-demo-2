c9b024817914ea58bf1948b0a07ff4e031bfe02f
David Tang <687449+iamdtang@users.noreply.github.com>
2023-04-29 23:09:06 -0700

    Revert "useSearchParams example"
    This reverts commit 17e5849edf3d5cecf28f02f90ac992d38d548e1e.

--- src/routes/Index.js
+++ src/routes/Index.js
@@ -1,29 +1,13 @@
-import { useLoaderData, useSearchParams, Form, Link } from "react-router-dom";
+import { useLoaderData, Form, Link } from "react-router-dom";
 
 export default function Index() {
   const hobbies = useLoaderData();
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  let filteredHobbies = hobbies;
-
-  if (searchParams.has("q")) {
-    const q = searchParams.get("q");
-
-    filteredHobbies = hobbies.filter((hobby) => {
-      return hobby.name.toLowerCase().includes(q.toLowerCase());
-    });
-  }
 
   return (
     <div>
       <h1>Hobbies</h1>
-
-      <Form method="get">
-        <input type="search" placeholder="Search for a hobby" name="q" />
-      </Form>
-
       <ul>
-        {filteredHobbies.map((hobby) => {
+        {hobbies.map((hobby) => {
           return (
             <li key={hobby.id}>
               <Link to={`/hobbies/${hobby.id}`}>{hobby.name}</Link>
